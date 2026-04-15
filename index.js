8d8db61a58ac4cc601b887e60a8e5d0f5fae3384
James Mounts <mounts10@gmail.com>
2026-04-15 17:21:16 -0600

    Modules Lab

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+require("datejs");
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
 
+  for (const arr of args) {
+    combinedObject.users = [...combinedObject.users, ...arr];
+  }
+
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+  return combinedObject;
+}
+
+module.exports = {
+  ...(typeof combineUsers !== "undefined" && { combineUsers })
+};
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
